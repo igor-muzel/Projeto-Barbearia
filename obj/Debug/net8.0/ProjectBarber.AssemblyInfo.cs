a9cf24c1b49d7fc5c48b2b5f8bf08f890b992885
igor-muzel <igor.muzel1998@gmail.com>
2026-05-25 22:46:23 -0300

    definindo cores para o nome do admin no 'header' e lógica do acesso do admin e usuario comum em Index (ao logar no sistema)

--- obj/Debug/net8.0/ProjectBarber.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectBarber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectBarber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5247f96f6e3566f8cc742b176ca26c5efc8fef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61fe5f7693f194091e4f0191f829538f3bab226b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectBarber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectBarber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
